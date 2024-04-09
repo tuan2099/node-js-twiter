c9911c236d5d6846107793fef974905ebae32152
tuan2099 <gwoqkepspsld@gmail.com>
2024-04-09 07:18:48 +0700

    commplete message err

--- src/middleware/users.middlewares.ts
+++ src/middleware/users.middlewares.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { checkSchema } from 'express-validator'
+import { USER_MESSAGE } from '~/constants/message'
 import userService from '~/services/users.services'
 import { validate } from '~/utils/validator'
 
@@ -13,80 +14,84 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
   next()
 }
 
-export const registerValidator = validate(checkSchema({
-  name: {
-    notEmpty: true,
-    isString: true,
-    trim: true,
-    isLength: {
-      options: {
-        min: 1,
-        max: 100,
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: {
+        errorMessage: USER_MESSAGE.NAME_IS_REQUIRED
       },
-      errorMessage: 'min value must be greater than 1 or greater than 100'
+      isString: {
+        errorMessage: USER_MESSAGE.NAME_MUST_BE_A_STRING
+      },
+      trim: true,
+      isLength: {
+        options: {
+          min: 1,
+          max: 100
+        },
+        errorMessage: USER_MESSAGE.NAME_LENGTH_MUST_BE_FROM_1_TO_100
+      }
     },
-    errorMessage: 'please enter a valid value'
-  },
-  email: {
-    notEmpty: true,
-    isEmail: true,
-    trim: true,
-    errorMessage: 'Please enter a valid email address',
-    custom: {
-      options: async (value) => {
-        const isExistsEmail = await userService.checkEmailExists(value);
-        if (isExistsEmail) {
-          throw new Error("Email already exists")
+    email: {
+      notEmpty: true,
+      isEmail: true,
+      trim: true,
+      errorMessage: 'Please enter a valid email address',
+      custom: {
+        options: async (value) => {
+          const isExistsEmail = await userService.checkEmailExists(value)
+          if (isExistsEmail) {
+            throw new Error('Email already exists')
+          }
+          return true
         }
-        return true
-      }
-    }
-  },
-  password: {
-    notEmpty: true,
-    isString: true,
-    isLength: {
-      options: {
-        min: 8,
-        max: 100
       }
     },
-    isStrongPassword: {
-      options: {
-        minLength: 6,
-        minLowercase: 1,
-        minNumbers: 1,
-        minSymbols: 1,
-        minUppercase: 1
-      }
-    },
-    custom: { // kiểm tra 2 pass có trùng nhau hay không
-      options: (value, { req }) => {
-        if (value !== req.body.password) {
-          throw new Error('Password confirmation dose not match password')
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 8,
+          max: 100
         }
-        return true
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+          minUppercase: 1
+        }
+      },
+      custom: {
+        // kiểm tra 2 pass có trùng nhau hay không
+        options: (value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Password confirmation dose not match password')
+          }
+          return true
+        }
+      }
+    },
+    confirmPassword: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 1,
+          max: 100
+        }
+      }
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
       }
     }
-  },
-  confirmPassword: {
-    notEmpty: true,
-    isString: true,
-    isLength: {
-      options: {
-        min: 1,
-        max: 100
-      }
-    }
-
-  },
-  date_of_birth: {
-    isISO8601: {
-      options: {
-        strict: true,
-        strictSeparator: true
-      }
-    }
-  }
-
-}))
+  })
+)
