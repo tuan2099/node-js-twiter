b376981f96dcb26c5613b3bea11a143df4952915
tuan2099 <gwoqkepspsld@gmail.com>
2025-02-03 15:12:18 +0700

    reset project

--- src/routes/static.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { serverImageController, serverVideoStreamController } from '~/controllers/media.controller'
-
-const staticRouter = Router()
-
-staticRouter.get('/image/:name', serverImageController)
-staticRouter.get('/video-stream/:name', serverVideoStreamController)
-
-export default staticRouter
